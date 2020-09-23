c20dc288ba692cebf1171090218d3ceab8e58cf6
MadMakZa <immakza@gmail.com>
2020-09-23 22:02:41 +0300

    update lesson 4-4

--- app/src/main/java/com/makza/fisher_hanbook/MainActivity.java
+++ app/src/main/java/com/makza/fisher_hanbook/MainActivity.java
@@ -1,6 +1,7 @@
 package com.makza.fisher_hanbook;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
@@ -10,6 +11,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.core.view.GravityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,11 +20,15 @@ import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private ListView list;
     private String[] array;
     private ArrayAdapter<String> adapter;
+    private Toolbar toolbar;
 
 
     @Override
@@ -31,13 +37,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         list = findViewById(R.id.listView); //присвоил переменной лист листвью
         array = getResources().getStringArray(R.array.fish_array); //присвоил список массиву
-        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,array);
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,new ArrayList<String>(Arrays.asList(array)));
         list.setAdapter(adapter); //присваиваем список спомощью адаптера
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        toolbar.setTitle(R.string.fish); //присваиваем заголовок с названием рыба
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
 
     }
@@ -46,6 +56,67 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item){
+        int id = item.getItemId();
+
+        if (id == R.id.id_fish){
+            toolbar.setTitle(R.string.fish);
+            array = getResources().getStringArray(R.array.fish_array); //присвоил список массиву
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged(); //говорим что данные изменились
+        }
+        else if (id == R.id.id_na){
+            toolbar.setTitle(R.string.na);
+            array = getResources().getStringArray(R.array.na_array); //присвоил список массиву
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
+        }
+        else if (id == R.id.id_sna){
+            toolbar.setTitle(R.string.sna);
+            array = getResources().getStringArray(R.array.sna_array); //присвоил список массиву
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
+        }
+        else if (id == R.id.id_pri){
+            toolbar.setTitle(R.string.pri);
+            array = getResources().getStringArray(R.array.pri_array); //присвоил список массиву
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
+        }
+        else if (id == R.id.id_history){
+            toolbar.setTitle(R.string.history);
+            array = getResources().getStringArray(R.array.history_array); //присвоил список массиву
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
+        }
+        else if (id == R.id.id_advice){
+            toolbar.setTitle(R.string.advice);
+            array = getResources().getStringArray(R.array.advice_array); //присвоил список массиву
+            adapter.clear();
+            adapter.addAll(array);
+            adapter.notifyDataSetChanged();
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START); //закрытие шторки
         return true;
     }
 
